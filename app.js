d9c7d86278ca1559732c6fbb130192466b5a59e1
RamaniRavi <ramaniravi196@gmail.com>
2021-04-03 10:02:19 +0530

    2nd commit

--- app.js
+++ app.js
@@ -7,35 +7,14 @@ app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
     var today = new Date();
-    var cday = today.getDay();
-    var day = "";
+   
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US",options);
 
-    switch (cday) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuseday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-
-        default:
-            break;
-    }
     res.render("list", {keyday: day});
 });
 
